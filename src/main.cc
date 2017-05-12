f75c29744dfccfe1e1ba34826c09ec867a169a8f
unknown <am244020@IS226224.intra.cea.fr>
2017-05-12 09:07:23 +0200

    OpenGLLearning:         Handling two models (VBO/VAO/EOB) and two shader program.

--- src/main.cc
+++ src/main.cc
@@ -14,7 +14,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
 	// When a user presses the escape key, we set the WindowShouldClose property to true,
 	// closing the application
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	if ((key == GLFW_KEY_ESCAPE  || key == GLFW_KEY_A) && action == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
 		std::cout << "Application is closing now." << std::endl;
@@ -36,7 +36,8 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 }
 
-void create_scene(GLuint* VBO, GLuint* VAO, GLuint* EBO)
+
+void create_scene(std::list<GLuint>* VBO_l, std::list<GLuint>* VAO_l, std::list<GLuint>* EBO_l)
 {
 	// Set up vertex data (and buffer(s)) and attribute pointers.
     GLfloat vertices[] = {-0.5f, 0.5f, 0.0f,
@@ -44,48 +45,57 @@ void create_scene(GLuint* VBO, GLuint* VAO, GLuint* EBO)
 						  0.5f,  -0.5f, 0.0f,
 						  -0.5f,  -0.5f, 0.0f};
 
-	GLuint indices[] = {0, 1, 2,
-		                2, 3, 0};
+	// FIXME: for test purposes.
+	GLuint indices[2][3] = {{0, 1, 2}, {2, 3, 0}};
+
+	// FIXME: can we do better?
+	auto elt_vbo = VBO_l->begin();
+	auto elt_vao = VAO_l->begin();
+	auto elt_ebo = EBO_l->begin();
+	unsigned int i = 0;
+	for (; elt_vbo != VBO_l->end(); ++elt_vbo, ++elt_vao, ++elt_ebo)
+	{
+		// Create one buffer object (our vertex buffer object).
+		glGenBuffers(1, &(*elt_vbo));
+
+		// Create one array object (our vertex array object).
+		// Our vertex array object will keep track (and store) every changes in the context caused by the VBO.
+		// To do all the drawing, we just have to bind the VAO and it's done.
+		glGenVertexArrays(1, &((*elt_vao)));
+
+		// To avoid duplication of the vertices, we will use indices to draw our triangles.
+		// To do so, we use an element object buffer. Every changes made the the EOB are also recorded in the VAO.
+		glGenBuffers(1, &((*elt_ebo)));
+
+		// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+		glBindVertexArray(*elt_vao);
+		glBindBuffer(GL_ARRAY_BUFFER, *elt_vbo);
+
+		// Fill the type array buffer with our vertices. Static data since we do not want it to move.
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+	
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *elt_ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[i]), indices[i], GL_STATIC_DRAW);
+	
+
+		// Unbind VAO by setting it to a null pointer 
+		// (it's always a good thing to unbind any buffer/array to prevent strange bugs).
+		glBindVertexArray(0);
+
+
+		++i;
+	}
 
 	
 
-	// Create one buffer object (our vertex buffer object).
-    glGenBuffers(1, VBO);
-	
-	// Create one array object (our vertex array object).
-	// Our vertex array object will keep track (and store) every changes in the context caused by the VBO.
-	// To do all the drawing, we just have to bind the VAO and it's done.
-    glGenVertexArrays(1, VAO);
-
-	// To avoid duplication of the vertices, we will use indices to draw our triangles.
-	// To do so, we use an element object buffer. Every changes made the the EOB are also recorded in the VAO.
-	glGenBuffers(1, EBO);
-
-
-	// FIXME: move it above no ?
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray(*VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, *VBO);
-
-	// Fill the type array buffer with our vertices. Static data since we do not want it to move.
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
 
 	
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	
-
-	
-
-	// Unbind VAO by setting it to a null pointer 
-	// (it's always a good thing to unbind any buffer/array to prevent strange bugs).
-    glBindVertexArray(0);
-
 
 }
 
@@ -189,12 +199,24 @@ std::list<std::pair<std::string, GLenum>> load_shader_file(std::list<std::pair<s
 
 int main(int argc, char* argv[])
 {
-	// This is our vertex buffer object.
-	GLuint VBO;
+	// This is our vertex buffer objects.
+	GLuint VBO1, VBO2;
 	// This is our vertex array object.
-	GLuint VAO;
-	// This is our element buffer object.
-	GLuint EBO;
+	GLuint VAO1, VAO2;
+	// This is our element buffer objects.
+	GLuint EBO1, EBO2;
+
+	std::list<GLuint> VAO_l;
+	VAO_l.push_back(VAO1);
+	VAO_l.push_back(VAO2);
+
+	std::list<GLuint> EBO_l;
+	EBO_l.push_back(EBO1);
+	EBO_l.push_back(EBO2);
+
+	std::list<GLuint> VBO_l;
+	VBO_l.push_back(VBO1);
+	VBO_l.push_back(VBO2);
 
 	// Init GLFW.
 	int error_code = 0;
@@ -207,20 +229,36 @@ int main(int argc, char* argv[])
 	if (error_code)
 		return error_code;
 	
-	std::list<std::pair<std::string, GLenum >> shader_l;
-	std::string fs("C:/Users/am237982/Desktop/AlexFormation/openGL/src/shader.fs");
+	// FIXME: cumbersome...
+	std::list<std::pair<std::string, GLenum >> shader_l1;
+	std::string fs("C:/Users/am237982/Desktop/AlexFormation/openGL/src/red.fs");
 	std::string vs("C:/Users/am237982/Desktop/AlexFormation/openGL/src/shader.vs");
-	shader_l.push_back(make_pair(fs, GL_FRAGMENT_SHADER));
-	shader_l.push_back(make_pair(vs, GL_VERTEX_SHADER));
+	shader_l1.push_back(make_pair(fs, GL_FRAGMENT_SHADER));
+	shader_l1.push_back(make_pair(vs, GL_VERTEX_SHADER));
 
-	std::list<std::pair<std::string, GLenum >> shader_code_l = load_shader_file(shader_l);
+	std::list<std::pair<std::string, GLenum >> shader_code_l1 = load_shader_file(shader_l1);
 
-	GLuint shaderProgram;
+	
+	std::list<std::pair<std::string, GLenum >> shader_l2;
+	fs = std::string("C:/Users/am237982/Desktop/AlexFormation/openGL/src/orange.fs");
+	vs = std::string("C:/Users/am237982/Desktop/AlexFormation/openGL/src/shader.vs");
+	shader_l2.push_back(make_pair(fs, GL_FRAGMENT_SHADER));
+	shader_l2.push_back(make_pair(vs, GL_VERTEX_SHADER));
+
+	std::list<std::pair<std::string, GLenum >> shader_code_l2 = load_shader_file(shader_l2);
+
+	GLuint shaderProgram1;
+	GLuint shaderProgram2;
 	// Handle the shaders, load them and bind them.
-	compile_shader(&shaderProgram, shader_code_l);
+	compile_shader(&shaderProgram1, shader_code_l1);
+	compile_shader(&shaderProgram2, shader_code_l2);
+
+	std::list<GLuint> shader_program_l;
+	shader_program_l.push_back(shaderProgram1);
+	shader_program_l.push_back(shaderProgram2);
 
 	// Create the scene and init the context (VBO, ...).
-	create_scene(&VBO, &VAO, &EBO);
+	create_scene(&VBO_l, &VAO_l, &EBO_l);
 
 	// Main loop.
 	while(!glfwWindowShouldClose(window))
@@ -232,19 +270,31 @@ int main(int argc, char* argv[])
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-		glUseProgram(shaderProgram);
-		// FIXME: the usage of the VAO is not clear to me.
-		// Do we use a VAO to avoid some cumbersome procedure ? (declare the vertex in the VBO and release it ?).
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glBindVertexArray(0);
+		auto elt_shader = shader_program_l.begin();
+		for (auto elt_vao = VAO_l.begin(); elt_vao != VAO_l.end(); ++elt_vao, ++ elt_shader)
+		{
+			glUseProgram((*elt_shader));
+			// FIXME: the usage of the VAO is not clear to me.
+			// Do we use a VAO to avoid some cumbersome procedure ? (declare the vertex in the VBO and release it ?).
+			glBindVertexArray(*elt_vao);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+		}
 
 		// Swap the buffer to avoid the flickering.
 		glfwSwapBuffers(window);
 	}
-    // Properly de-allocate all resources once they've outlived their purpose
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+
+	auto elt_vao = VAO_l.begin();
+	auto elt_vbo = VBO_l.begin();
+	auto elt_ebo = EBO_l.begin();
+	for (; elt_vao != VAO_l.end(); ++elt_vao, ++elt_vbo, ++elt_ebo)
+	{
+		// Properly de-allocate all resources once they've outlived their purpose
+		glDeleteVertexArrays(1, &(*elt_vao));
+		glDeleteBuffers(1, &(*elt_vbo));
+		glDeleteBuffers(1, &(*elt_ebo));
+	}
 
 	glfwTerminate();
 	return error_code;
